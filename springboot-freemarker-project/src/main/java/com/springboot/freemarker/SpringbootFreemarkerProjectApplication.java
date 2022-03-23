ad59436d2234b5379313e79718f73927c3de4007
raniaw@web.de <raniaw@web.de>
2022-03-23 11:40:37 +0100

    changed Application

--- springboot-freemarker-project/src/main/java/com/springboot/freemarker/SpringbootFreemarkerProjectApplication.java
+++ springboot-freemarker-project/src/main/java/com/springboot/freemarker/SpringbootFreemarkerProjectApplication.java
@@ -1,13 +1,27 @@
 package com.springboot.freemarker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringbootFreemarkerProjectApplication {
+import com.springboot.freemarker.model.Employee;
+import com.springboot.freemarker.repository.EmployeeRepository;
 
+@SpringBootApplication
+public class SpringbootFreemarkerProjectApplication implements CommandLineRunner {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootFreemarkerProjectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		employeeRepository.save(new Employee("Joe","Dove","joe.dove@gmail.com"));
+		employeeRepository.save(new Employee("Marina","Sark","marina.sark@gmail.com"));
+		employeeRepository.save(new Employee("Joern","Boern","joern.boern@gmail.com"));
 	}
 
 }
--- springboot-freemarker-project/src/main/java/com/springboot/freemarker/repository/EmployeeRepository.java
+++ springboot-freemarker-project/src/main/java/com/springboot/freemarker/repository/EmployeeRepository.java
@@ -3,6 +3,8 @@ package com.springboot.freemarker.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.springboot.freemarker.model.Employee;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Long>{
 
